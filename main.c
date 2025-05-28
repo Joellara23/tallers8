18389d151281fa99621be3fceaf3d21b10664b25
Joellara23 <joellara14@hotmail.com>
2025-05-28 17:09:01 -0500

    Update main.c

--- main.c
+++ main.c
@@ -4,23 +4,22 @@
 
 #define MAX 5
 
-char nombres[MAX][20];
-int tiempos[MAX];
-int recursos[MAX];
-int cantidades[MAX];
-int total = 0;
-
 int main() {
+    char nombres[MAX][20];
+    int tiempos[MAX];
+    int recursos[MAX][3];
+    int cantidades[MAX];
+    int total = 0;
     int opcion;
 
     do {
-        printf("\n--- Menu ---\n");
+        printf("\nMenu:\n");
         printf("1. Ingresar producto\n");
-        printf("2. Editar producto\n");
-        printf("3. Eliminar producto\n");
-        printf("4. Calcular produccion\n");
-        printf("5. Mostrar productos\n");
-        printf("6. Salir\n");
+        printf("2. Mostrar productos\n");
+        printf("3. Editar producto\n");
+        printf("4. Eliminar producto\n");
+        printf("5. Calcular produccion\n");
+        printf("0. Salir\n");
         opcion = leerEnteroPositivo("Opcion: ");
 
         switch (opcion) {
@@ -28,24 +27,24 @@ int main() {
                 ingresar(nombres, tiempos, recursos, cantidades, &total, MAX);
                 break;
             case 2:
-                editar(nombres, tiempos, recursos, cantidades, total);
-                break;
-            case 3:
-                eliminar(nombres, tiempos, recursos, cantidades, &total);
-                break;
-            case 4:
-                calcular(nombres, tiempos, recursos, cantidades, total);
-                break;
-            case 5:
                 mostrar(nombres, tiempos, recursos, cantidades, total);
                 break;
-            case 6:
-                printf("Programa finalizado.\n");
+            case 3:
+                editar(nombres, tiempos, recursos, cantidades, total);
+                break;
+            case 4:
+                eliminar(nombres, tiempos, recursos, cantidades, &total);
+                break;
+            case 5:
+                calcular(nombres, tiempos, recursos, cantidades, total);
+                break;
+            case 0:
+                printf("Saliendo...\n");
                 break;
             default:
                 printf("Opcion invalida.\n");
         }
-    } while (opcion != 6);
+    } while (opcion != 0);
 
     return 0;
 }
